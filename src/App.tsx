c05dd35bf1fca43505fc16e37e01633cec75ecad
whoiskouti <mortezamalakouti653@gmail.com>
2025-07-24 16:40:05 +0330

    intitial folders

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { HeroSection } from '@/components/ui/hero-section';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white">
+      <HeroSection />
+    </div>
+  );
+}
+
+export default App;
